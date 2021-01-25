64b9d996b11bc241882933aa75724394fd0e9bdc
jin-Pro <dnjun2@naver.com>
2021-01-25 14:17:33 +0900

    2021-01-25 with nomadcoders

--- 노마드코더/index.js
+++ 노마드코더/index.js
@@ -1,1 +1,16 @@
-alert("I'm Working. I'm JS. I'm Beautiful. I'm worth it");
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+const hasClass = title.classList.contains(CLICKED_CLASS);
+
+const changeColor = function(){
+
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+function init(){
+    title.addEventListener('click',changeColor);
+}
+
+init();
